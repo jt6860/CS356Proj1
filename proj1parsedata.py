d5e20eeba93ac813130b0ccbc8bb13d27fe7aef9
jt6860 <torj16@protonmail.com>
2023-12-14 12:50:32 -0500

    Transfer of Python code and snip from hd.

---
+++ proj1parsedata.py
@@ -0,0 +1,47 @@
+name1 = "Center/Daycare"
+streetAddress1 = "825 23rd Street South"
+cityState1 = "Arlington, VA"
+zip1 = "22202"
+phone1 = "703-979-BABY (2229)"
+entrynum1 = "22"
+website1 = "www.mariateresasbabies.com"
+childAges1 = "6 wks to 5yrs"
+timeframe1 = "Full-time"
+owner1 = "Maria Teresa Desaba, Owner/Director"
+director1 = "Tony Saba, Org. Director"
+
+name2 = "National Science Foundation Child Development Center"
+streetAddress2 = "4201 Wilson Blvd., Suite 180"
+cityState2 = "Arlington, VA"
+zip2 = "22203"
+phone2 = "703-292-4794"
+entrynum2 = "23"
+website2 = "www.brighthorizons.com"
+childAges2 = "ages 6 wks–5 yrs, Summer Camp for children 5–9 years"
+timeframe2 = "7:00 a.m. – 6:00 p.m."
+numberChildren = "112 children"
+
+print("ENTRY #: " + entrynum1)
+print("NAME: " + name1)
+print("ADDRESS: " + streetAddress1)
+print("CITY/STATE: " + cityState1)
+print("ZIP CODE: " + zip1)
+print("PHONE: " + phone1)
+print("WEBSITE: " + website1)
+print("AGES SERVED: " + childAges1)
+print("TIMEFRAME: " + timeframe1)
+print("OWNER: " + owner1)
+print("DIRECTOR: " + director1)
+
+print("")
+
+print("ENTRY #: " + entrynum2)
+print("NAME: " + name2)
+print("ADDRESS: " + streetAddress2)
+print("CITY/STATE: " + cityState2)
+print("ZIP CODE: " + zip2)
+print("PHONE: " + phone2)
+print("WEBSITE: " + website2)
+print("AGES SERVED: " + childAges2)
+print("TIMEFRAME: " + timeframe2)
+print("# OF CHILDREN: " + numberChildren)
